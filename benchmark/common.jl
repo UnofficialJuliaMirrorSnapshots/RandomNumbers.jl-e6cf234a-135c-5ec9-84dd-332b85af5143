62ff1804c57fc3952db5688763604ab213b8b840
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 01:59:07 +0000

    Snapshot of branch master taken on 2019-04-07T21:59:05.882-04:00 from "https://github.com/UnofficialJuliaMirror/RandomNumbers.jl-e6cf234a-135c-5ec9-84dd-332b85af5143"

---
+++ benchmark/common.jl
@@ -0,0 +1,43 @@
+try
+    using RNGTest
+catch
+    warn("No RNGTest package found. Only speed benchmarks can be run.")
+end
+using RandomNumbers
+import Printf: @printf
+
+function bigcrush{T<:Number}(rng::RandomNumbers.AbstractRNG{T})
+    p = RNGTest.wrap(r, T)
+    RNGTest.bigcrushTestU01(p)
+end
+
+function speed_test{T<:Number}(rng::RandomNumbers.AbstractRNG{T}, n=100_000_000)
+    A = Array{T}(n)
+    rand!(rng, A)
+    elapsed = @elapsed rand!(rng, A)
+    elapsed * 1e9 / n * 8 / sizeof(T)
+end
+
+function bigcrush(rng::MersenneTwister)
+    p = RNGTest.wrap(r, UInt64)
+    RNGTest.bigcrushTestU01(p)
+end
+
+function speed_test(rng::MersenneTwister, n=100_000_000)
+    T = UInt64
+    A = Array{T}(n)
+    rand!(rng, A)
+    elapsed = @elapsed rand!(rng, A)
+    elapsed * 1e9 / n * 8 / sizeof(T)
+end
+
+function test_all(rng::Base.AbstractRNG, n=100_000_000)
+    fo = open("$TEST_NAME.log", "w")
+    redirect_stdout(fo)
+    println(TEST_NAME)
+    speed = speed_test(rng, n)
+    @printf "Speed Test: %.3f ns/64 bits\n" speed
+    flush(fo)
+    bigcrush(rng)
+    close(fo)
+end
